582fd80b69499674ef88d4da3fdeb165403ed4e1
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-12-19 16:34:09 +0200

    Update project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import MetricCardOne from '@/components/sections/metrics/MetricCardOne';
 import TestimonialCardTwo from '@/components/sections/testimonial/TestimonialCardTwo';
 import ContactSplitForm from '@/components/sections/contact/ContactSplitForm';
 import FooterSocial from '@/components/sections/footer/FooterSocial';
-import { Sparkles, Award, Binoculars, Heart, Leaf, BookOpen, Users, TrendingUp, Calendar, PawPrint, Star, Facebook, Instagram, Twitter, Youtube } from "lucide-react";
+import { Sparkles, Award, Binoculars, Heart, Leaf, BookOpen, Users, TrendingUp, Calendar, Star, Facebook, Instagram, Twitter, Youtube } from "lucide-react";
 
 export default function LandingPage() {
   return (
@@ -198,7 +198,7 @@ export default function LandingPage() {
               value: "6000",
               title: "Animals",
               description: "From over 700 different species around the world",
-              icon: PawPrint
+              icon: Users
             },
             {
               id: "3",
@@ -314,7 +314,6 @@ export default function LandingPage() {
         <FooterSocial
           logoText="Zoo Madrid"
           logoSrc="https://webuild-dev.s3.eu-north-1.amazonaws.com/gallery/uploaded-1766139811510-n9knt0v0.jpg"
-          logoAlt="Zoo Madrid"
           columns={[
             {
               title: "Visitor Info",
