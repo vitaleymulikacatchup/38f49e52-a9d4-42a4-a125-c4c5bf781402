d3287069c5b0a0b3a266dde075e4dacebd1a0720
vitaleymulikacatchup <vitaley.mulika.catchup@gmail.com>
2025-12-19 17:21:31 +0200

    Update project files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -358,6 +358,9 @@ export default function LandingPage() {
             { icon: Youtube, href: "https://youtube.com/zoomadrid", ariaLabel: "Zoo Madrid YouTube" }
           ]}
           copyrightText="© Zoo Madrid 2025. All rights reserved."
+          footerClassName="bg-green-900"
+          className="bg-green-900"
+          containerClassName="bg-green-900"
         />
       </div>
     </ThemeProvider>
